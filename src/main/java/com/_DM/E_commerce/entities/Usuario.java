61150985f90db048573f26866145ef757f771da5
GoBrazill <vinicius.daraujos26@gmail.com>
2026-04-15 10:31:46 +0000

    refactor: make enum be converted to string in the database

--- src/main/java/com/_DM/E_commerce/entities/Usuario.java
+++ src/main/java/com/_DM/E_commerce/entities/Usuario.java
@@ -26,6 +26,7 @@ public class Usuario {
     private String email;
     private String telefone;
     private String senha;
+    @Enumerated(EnumType.STRING)
     private Role role;
 
     @OneToMany(mappedBy = "cliente")
