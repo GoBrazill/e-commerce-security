44d9b4dcb34a310a03851fb2663dccde9f67fec7
GoBrazill <vinicius.daraujos26@gmail.com>
2026-04-29 11:02:07 +0000

    feat: add authentication manager config and use jwt filter for authenticaiton

--- src/main/java/com/_DM/E_commerce/config/SecurityConfig.java
+++ src/main/java/com/_DM/E_commerce/config/SecurityConfig.java
@@ -2,32 +2,45 @@ package com._DM.E_commerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
 
-        http.csrf(csrf -> csrf.disable())
+        return http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/usuario/criar").permitAll()
+                        .requestMatchers("/usuario/criar", "/login").permitAll()
                         .requestMatchers("/usuario/admin", "/usuario/mostrar/**","/usuario/atualizar/", "/usuario/deletar/").hasRole("ADMIN")
                         .anyRequest().authenticated()
-                ).httpBasic(withDefaults());
+                ).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class).build();
 
-        return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
+        return config.getAuthenticationManager();
     }
 }
